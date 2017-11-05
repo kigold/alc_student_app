2d963d55f7deb81696c711c3e55ba9d40b7d2a3f
kingslee <kingsleybox@yahoo.com>
2017-11-05 02:28:27 +0100

    added header to express server

--- server/studentapp/app.js
+++ server/studentapp/app.js
@@ -11,6 +11,18 @@ var users = require('./routes/users');
 
 var app = express();
 
+
+//CORS ish
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', "*");
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+  next();
+}
+
+
+app.use(allowCrossDomain);
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
